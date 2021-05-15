2d45bcadacb11cfe6dae15c61ec30ca356ffac2f
Jennysiq <eugensema1@gmail.com>
2021-05-15 23:05:05 +0300

    fix infr

--- main.tf
+++ main.tf
@@ -1,52 +1,66 @@
 provider "aws" {
-    region     = "us-east-1"
+  region = "us-east-1"
+}
 
-data "aws_instances" "webserver_instans" {
-  instance_tags = {
-    Name = "FlaskAPP"
+locals {
+  user_data = <<EOF
+!#/bin/bash
+echo "DRASTE-MARDASTE"
+EOF
+}
+
+##############DATA SOURCE VPC,Subnet,SG,AMI####################
+data "aws_vpc" "default" {
+  default  = true
+}
+
+data "aws_subnet_ids" "all" {
+  vpc_id   = data.aws_vpc.default.id
+}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners   = [ "amazon" ]
+  
+  filter{
+    name   = "name"
+
+    values = ["amzn-ami-hvm-*x86_64-gp2",
+    ]
   }
 
-  filter {
-      name   = "tag:Name"
-      values = ["FlaskAPP"]
+  filter{
+    name   = "owner-jen"
+
+    values = [ "amazon", 
+    ]
   }
 }
 
-output "aws_instans_public_ip" {
-    value = data.aws_instances.webserver_instans.public_ips
+module "security_group" {
+  source              = "terraform-aws-module/security-group/aws"
+  version             = "~ 4.0"
+
+  name                = "myflask"
+  description         = "SG for flask"
+  vpc_id              = data.aws_vpc.default.vpc_id
+
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_rules       = ["http-80-tcp", "http-22-tcp", "http-5000-tcp"]
+  egress_rules        = ["all-all"]
 }
 
-resource "aws_security_group" "webSG" {
-  name = "Dynamic Security Group"
+module "ec2_with_t2_unlimited" {
+  source = "../../"
 
-  dynamic "ingress" {
-    for_each = ["80","22", "5000"]
-    content {
-      from_port   = ingress.value
-      to_port     = ingress.value
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  instance_count = 2
 
-  tags = {
-    Name  = "Dymanic SG for APP"
-  }
-}
-
-resource "random_string" "random" {
-  length = 16
-  special = false
-  min_lower = 16
-}
-
-resource "aws_kms_key" "this" {}
-resource "aws_s3_bucket" "that" {
-  bucket = "${random_string.random.result}"
+  name          = "example-t2-unlimited"
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t2.micro"
+  cpu_credits   = "unlimited"
+  key_name      = "jenkins"
+  subnet_id     = tolist(data.aws_subnet_ids.all.ids)[0]
+  vpc_security_group_ids      = [module.security_group.security_group_id]
+  associate_public_ip_address = true
 }
