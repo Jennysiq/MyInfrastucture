1f0a45b2504bddaea00f51bc367201b3bc898b3b
Jennysiq <eugensema1@gmail.com>
2021-05-16 00:49:43 +0300

    add full infr

--- main.tf
+++ main.tf
@@ -20,42 +20,44 @@ data "aws_subnet_ids" "all" {
 
 data "aws_ami" "amazon_linux" {
   most_recent = true
-  owners   = [ "amazon" ]
-  
-  filter{
-    name   = "name"
 
-    values = ["amzn-ami-hvm-*x86_64-gp2",
+  owners = ["amazon"]
+
+  filter {
+    name = "name"
+
+    values = [
+      "amzn-ami-hvm-*-x86_64-gp2",
     ]
   }
 
-  filter{
-    name   = "owner-jen"
+  filter {
+    name = "owner-alias"
 
-    values = [ "amazon", 
+    values = [
+      "amazon",
     ]
   }
 }
 
 module "security_group" {
-  source              = "terraform-aws-module/security-group/aws"
-  version             = "~ 4.0"
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "~> 4.0"
 
-  name                = "myflask"
-  description         = "SG for flask"
-  vpc_id              = data.aws_vpc.default.vpc_id
+  name        = "MySGFlask"
+  description = "Security group for my E2C flask APP"
+  vpc_id      = data.aws_vpc.default.id
 
   ingress_cidr_blocks = ["0.0.0.0/0"]
-  ingress_rules       = ["http-80-tcp", "http-22-tcp", "http-5000-tcp"]
+  ingress_rules       = ["http-80-tcp", "all-icmp"]
   egress_rules        = ["all-all"]
 }
 
 module "ec2_with_t2_unlimited" {
-  source = "../../"
-
+  source = "terraform-aws-modules/ec2-instance/aws"
   instance_count = 2
 
-  name          = "example-t2-unlimited"
+  name          = "myapp"
   ami           = data.aws_ami.amazon_linux.id
   instance_type = "t2.micro"
   cpu_credits   = "unlimited"
@@ -63,4 +65,9 @@ module "ec2_with_t2_unlimited" {
   subnet_id     = tolist(data.aws_subnet_ids.all.ids)[0]
   vpc_security_group_ids      = [module.security_group.security_group_id]
   associate_public_ip_address = true
+  
+  tags          = {
+    Name        = "Myapp"
+    Owner       = "Jennysiq"
+  }
 }
