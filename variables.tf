fa8937651074fc3dfdf00a8f059d2544b7580ba1
Jennysiq <61979170+Jennysiq@users.noreply.github.com>
2021-05-18 11:11:33 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -154,3 +154,13 @@ variable "ubuntu_account_number" {
 variable "aws_region" {
     default = "us-east-1"
 }
+
+
+variable "ami_id" {
+    // Ubuntu Server 18.04 LTS (HVM), SSD Volume Type in us-west-2 
+    default = "ami-0d1cd67c26f5fca19"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
